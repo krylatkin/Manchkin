010a791e3cd79b19cdfdb69a5abbb0f595f8b592
krylatkin <krylatkin@bolid.ru>
2016-08-19 19:41:15 +0300

    Translate test into English

--- test/test.js
+++ test/test.js
@@ -1,14 +1,8 @@
 var player1;
 
-	describe("Создание и изменение персонажа", function() {
+	describe("Player. Create and Change", function() {
 
-		// before(function() { alert("Начало тестов"); });
-		// after(function() { alert("Конец тестов"); });
-
-		// beforeEach(function() { alert("Вход в тест"); });
-		// afterEach(function() { alert("Выход из теста"); });
-
-		it("Создание игрока", function() {
+		it("Create and Customise Player ", function() {
 			player1 = new App.Player({name: "PL1", sex: "male", elem:"#player1-holder"});
 			assert.equal(player1.char.name, 'PL1');
 			assert.equal(player1.char.sex, 'male');
@@ -16,43 +10,43 @@ var player1;
 			assert.equal(player1.char.charClass, 'none');
 		});
 
-		it("Измение расы", function() {
+		it("Change Race", function() {
 			assert.equal(player1.setRace('elf').race, 'elf');
 		});
 
-		it("Измение класса", function() {
+		it("Change Class", function() {
 			assert.equal(player1.setCharClass('warrior').charClass, 'warrior');
 		});
 
-		it("Измение пола", function() {
+		it("Change Sex", function() {
 			assert.equal(player1.setSex('female').sex, 'female');
 		});
 	});
 
-	describe("Уровень", function() {
+	describe("Player's Level", function() {
 
-		it("Узнать уровень", function() {
+		it("Get Level", function() {
 			assert.equal(player1.char.level, 1);
 		});
 
-		it("Измение уровня на 2", function() {
+		it("Set Level + 2", function() {
 			// player1.updateLevel(2);
 			// assert.equal(player1.level, 3);
 			assert.equal(player1.updateLevel(2).level, 3);
 		});
 
-		it("Измение уровня на -4", function() {
+		it("Set level -4", function() {
 			assert.equal(player1.updateLevel(-4).level, 1);
 		});
 	});
 
-	describe("Карты в руке", function() {
+	describe("Cards in Hand", function() {
 
-		it("Начальное кол-во", function() {
+		it("Initial count", function() {
 			assert.equal(player1.getHandCount(), 0);
 		});
 
-		it("Берем 2 карты", function() {
+		it("Take 2 cards", function() {
 			player1.addItem(item1);
 			player1.addItem(item2);
 			assert.equal(player1.getHandCount(), 2);
@@ -60,135 +54,135 @@ var player1;
 
 	});
 
-	describe("Инвентарь", function() {
+	describe("Cards in Inventory", function() {
 
-		it("Начальное кол-во", function() {
+		it("Initial count", function() {
 			assert.equal(player1.getInventoryCount(), 0);
 		});
 
-		it("Начальная мощность", function() {
+		it("Initial Power", function() {
 			assert.equal(player1.getPower(), 1);
 		});
 
-		it("Увеличиваем уровень на 2 и проверяем мощность", function() {
+		it("Set Level + 2 and Get Power", function() {
 			assert.equal(player1.updateLevel(2).getPower(), 3);
 		});
 
-		it("Одеваем шмотку. Проверяем кол-во", function() {
+		it("Wear clothes. Get count of clothes", function() {
 			player1.wearItem(item1);
 			assert.equal(player1.getInventoryCount(), 1);
 		});
 
-		it("Перепроверяем мощность +2", function() {
+		it("Check Power. Must be + 2", function() {
 			assert.equal(player1.getPower(), 5);
 		});
 
-		describe("Проверка шмота с условиями классов и рас", function() {
-			it("Одеваем шмотку. Только для эльфа. Мы эльфы. Проверяем кол-во", function() {
+		describe("Try to wear clothes with Class / Race conditions", function() {
+            it("Elf only. We are Elf", function() {
 				player1.wearItem(item2);
 				assert.equal(player1.getInventoryCount(), 2);
 			});
-			it("Перепроверяем мощность +5", function() {
+			it("Check Power. Must be + 5", function() {
 				assert.equal(player1.getPower(), 10);
 			});
-			it("Меняем расу. Пробуем надеться шмотку для эльфа. Проверяем кол-во", function() {
+			it("Change Race. We are not Elf. Elf only. Get count of clothes", function() {
 				player1.setRace('human');
 				player1.wearItem(item2);
 				assert.equal(player1.getInventoryCount(), 2);
 			});
-			it("Пробуем надеться шмотку для wizard. Проверяем кол-во", function() {
+			it("Wizard only. Get count of clothes", function() {
 				player1.wearItem(item3);
 				assert.equal(player1.getInventoryCount(), 2);
 			});
-			it("Меняем класс на wizard. Пробуем надеться шмотку для wizard. Проверяем кол-во", function() {
+			it("Change Class to Wizard. Wizard only. Get count of clothes", function() {
 				player1.setCharClass('wizard');
 				player1.wearItem(item3);
 				assert.equal(player1.getInventoryCount(), 3);
 			});
-			it("Перепроверяем мощность +15", function() {
+			it("Check Power. Must be + 15", function() {
 				assert.equal(player1.getPower(), 25);
 			});
 		});
 
-		describe("Проверка шмота со слотами", function() {
-			it("Одеваем бронник", function() {
+		describe("Try to wear clothes with Slot conditions", function() {
+			it("Armor", function() {
 				player1.wearItem(armor1);
 				assert.equal(player1.getInventoryCount(), 4);
 				assert.equal(player1.getPower(), 27);
 			});
-			it("Одеваем бронник еще раз. На этот раз не получится.", function() {
+			it("Armor one more time. At this time we can't", function() {
 				player1.wearItem(armor1);
 				assert.equal(player1.getInventoryCount(), 4);
 				assert.equal(player1.getPower(), 27);
 			});
-			it("Одеваем двуручное оружие", function() {
+			it("Two-hand weapon", function() {
 				player1.wearItem(twohand1);
 				assert.equal(player1.getInventoryCount(), 5);
 			});
-			it("Одеваем двуручное оружие еще раз. На этот раз не получится.", function() {
+			it("Two-hand weapon one more time. At this time we can't", function() {
 				player1.wearItem(twohand1);
 				assert.equal(player1.getInventoryCount(), 5);
 			});
-			it("Одеваем большой предмет", function() {
+			it("Big clothes", function() {
 				player1.wearItem(big1);
 				assert.equal(player1.getInventoryCount(), 6);
 			});
-			it("Одеваем большой предмет еще раз. На этот раз не получится.", function() {
+			it("Big clothes. At this time we can't", function() {
 				player1.wearItem(big2);
 				assert.equal(player1.getInventoryCount(), 6);
 			});
-			it("Меняем расу на Дварф. И одеваем большой предмет еще 2 раза.", function() {
+			it("Change Race to Dwarf. And we can wear Big clothes two more times", function() {
 				player1.setRace('dwarf');
 				player1.wearItem(big2);
 				player1.wearItem(big2);
 				assert.equal(player1.getInventoryCount(), 8);
 			});
-			it("Очищаем весь инвентарь", function() {
+			it("Clear Inventory", function() {
 				player1.clearInventory();
 				assert.equal(player1.getInventoryCount(), 0);
 				assert.equal(player1.getPower(), player1.char.level);
 			});
-			it("Одеваем одноручное оружие", function() {
+			it("One-hand weapon", function() {
 				player1.wearItem(hand1);
 				assert.equal(player1.getInventoryCount(), 1);
 			});
-			it("Одеваем одноручное оружие. Второй раз можно", function() {
+			it("One-hand weapon. We can second time", function() {
 				player1.wearItem(hand1);
 				assert.equal(player1.getInventoryCount(), 2);
 			});
-			it("Одеваем одноручное оружие. Третий раз уже нельзя", function() {
+			it("One-hand weapon. We cannot third time", function() {
 				player1.wearItem(hand1);
 				assert.equal(player1.getInventoryCount(), 2);
 			});
-			it("Одеваем двуручное оружие. В руках уже 2 оружия. Неполучится.", function() {
+			it("Two-hand weapon. We have already worn 2 One-hand weapons. We can't", function() {
 				player1.wearItem(twohand1);
 				assert.equal(player1.getInventoryCount(), 2);
 			});
-			it("Очищаем весь инвентарь", function() {
+			it("Clear Inventory", function() {
 				player1.clearInventory();
 				assert.equal(player1.getInventoryCount(), 0);
 				assert.equal(player1.getPower(), player1.char.level);
 			});
-			it("Одеваем двуручное оружие", function() {
+			it("Two-hand weapon", function() {
 				player1.wearItem(twohand1);
 				assert.equal(player1.getInventoryCount(), 1);
 			});
 
-			it("Одеваем одноручное оружие. Уже одето двуручное. Поэтому не получится", function() {
+			it("One-hand weapon. We have already worn Two-hand weapon. We can't", function() {
 				// console.clear();
 				player1.wearItem(hand1);
 				assert.equal(player1.getInventoryCount(), 1);
 			});
-			it("Очищаем весь инвентарь", function() {
+			it("Clear Inventory", function() {
 				player1.clearInventory();
 				assert.equal(player1.getInventoryCount(), 0);
 				assert.equal(player1.getPower(), player1.char.level);
 			});
-			it("Одеваем одноручное оружие", function() {
+			it("One-hand weapon", function() {
 				player1.wearItem(hand1);
 				assert.equal(player1.getInventoryCount(), 1);
 			});
-			it("Одеваем двуручное оружие. В руках 1 оружие, но все равно неполучится.", function() {
+			it("Two-hand weapon. We have already worn One-hand weapon. We can't.", function() {
 				player1.wearItem(twohand1);
 				assert.equal(player1.getInventoryCount(), 1);
 			});
@@ -197,28 +191,28 @@ var player1;
 
 	});
 
-	describe("Сражение с монстром", function() {
+	describe("Fight Monster. Compare Power.", function() {
 		player2 = new App.Player({name: "PL2", elem:"#player2-holder"});
 
-		it("Сравнить Player2 силы с Monster 1. Уровень 1 без шмота. Не справимся", function() {
+		it("Compare Player2 with Monster 1. Level 1 w/o clothes. We can't", function() {
 			assert.equal(player2.match(monster1), false);
 		});
-		it("Сравнить Player2 силы с Monster 1. Оденем и попробуем еще.", function() {
+		it("Compare Player2 with Monster 1. With clothes we can", function() {
 			player2.wearItem(item4);
 			assert.equal(player2.match(monster1), true);
 		});
-		it("Сравнить Player1 силы с Monster 1", function() {
+		it("Compare Player1 with Monster 1. We can", function() {
 			assert.equal(player1.match(monster1), true);
 		});
-		it("Сравнить Player1 силы с Monster 2", function() {
+		it("Compare Player1 with Monster 2. We can", function() {
 			assert.equal(player1.match(monster1), true);
 		});
-		it("Победить Monster 2", function() {
+		it("Player1 defeat Monster 2. And Get Level + 2", function() {
 			player1.win(monster1);
 			assert.equal(player1.char.level, 5);
-			// Получить кол-во win.treasure
+            // Get treasures in amount of monster.win.treasure
 		});
-		it("Победить Monster 2. Player2 не справиться.", function() {
+		it("Player2 defeat Monster 2. We can't. Level don't changed.", function() {
 			player2.win(monster2);
 			assert.equal(player2.char.level, 1);
 		});
